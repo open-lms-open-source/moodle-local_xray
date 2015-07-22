6b6e4722260012e082c31233e81263a1657bbbf4
gvitale <german@nxgened.com>
2015-07-22 15:24:14 -0300

    Slide-jssor: discussion by user

--- jquery/plugins.php
+++ jquery/plugins.php
@@ -14,30 +14,42 @@ $plugins = array(
         	'dataTables/jquery.dataTables.min.css'
         )
      ),    
-	 'local_xray-fancybox2' => array(
+    'local_xray-fancybox2' => array(
         'files' => array(
             'fancybox2/jquery.fancybox.js', 
             'fancybox2/jquery.fancybox.css'
         )
      ),    
-	 'local_xray-list_reports' => array(
+    'local_xray-list_reports' => array(
         'files' => array(
             'list_reports.js'
         )
-     ),    
-	 'local_xray-show_on_lightbox' => array(
+    ),
+    'local_xray-show_on_lightbox' => array(
         'files' => array(
             'show_on_lightbox.js'
         )
-     ),    
-	 'local_xray-itemBrowser' => array(
+    ),
+    'local_xray-itemBrowser' => array(
         'files' => array(
             'itembrowser/itemBrowser.js',
             'itembrowser/itemBrowser.css',
             'ContentFlow/contentflow.js',
             'ContentFlow/contentflow.css',
-        	'ContentFlow/mycontentflow.css',
+            'ContentFlow/mycontentflow.css',
             'show_modal_xrayformat.js',
         )
-     )
+    ),
+    'local_xray-jssor' => array(
+        'files' => array(
+            'jssor/jssor.js',
+            'jssor/jssor.slider.js',
+            'jssor/jssor.slider.min.js',
+        )
+    ),
+    'local_xray-image_gallery_with_vertical_thumbnail' => array(
+        'files' => array(
+            'image_gallery_with_vertical_thumbnail.js',
+        )
+    )
 );
