ab54715e9c4184be4403ea70d3b2f5cd97d94462
German Vitale <german@german-VAIO>
2015-07-28 16:36:42 -0300

    INT-7954: delete old discussion report

--- classes/reports_utils.php
+++ classes/reports_utils.php
@@ -15,8 +15,7 @@ class local_xray_reports_utils {
      */
     static function list_reports() {
         return array(array("activityreport", get_string("activityreport", "local_xray")),
-        		     array("activityreportindividual", get_string("activityreportindividual", "local_xray")),
-		             array("discussion_by_user", get_string("report_discussion_by_user", "local_xray")),
+                     array("activityreportindividual", get_string("activityreportindividual", "local_xray")),
                      array("discussionreport", get_string("discussionreport", "local_xray")),
                      array("discussionreportindividual", get_string("discussionreportindividual", "local_xray")),
         );
