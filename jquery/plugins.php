fd1700c11b8e0308e94499af348907d7037f3394
Pablo Pagnone <pablo@nxgened.com>
2015-08-06 11:43:04 -0300

    INT-7954: Clear old code

--- jquery/plugins.php
+++ jquery/plugins.php
@@ -14,31 +14,6 @@ $plugins = array(
             'fancybox2/jquery.fancybox.css'
         )
      ),    
-	'local_xray_activityreport_students_activity' => array(
-			'files' => array(
-					'reports/activityreport_students_activity.js'
-			)
-	),
-    'local_xray_discussionreport_participation_metrics' => array(
-            'files' => array(
-                    'reports/discussionreport_participation_metrics.js'
-            )
-    ),
-    'local_xray_discussionreport_discussion_activity_by_week' => array(
-            'files' => array(
-                    'reports/discussionreport_discussion_activity_by_week.js'
-            )
-    ),
-	'local_xray_activityreport_first_login_non_starters' => array(
-			'files' => array(
-					'reports/activityreport_first_login_non_starters.js'
-			)
-	),		
-	'local_xray_risk_risk_measures' => array(
-			'files' => array(
-					'reports/risk_risk_measures.js'
-			)
-	),
     'local_xray-show_on_lightbox' => array(
         'files' => array(
             'reports/show_on_lightbox.js'
