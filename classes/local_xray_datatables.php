dc216286d0ef5104b10546b3ece0e194264567a6
gvitale <german@nxgened.com>
2015-09-01 15:55:09 -0300

    INT-7954: local_xray_datatableColumn code style

--- classes/local_xray_datatables.php
+++ classes/local_xray_datatables.php
@@ -20,56 +20,56 @@
  * @author Pablo Pagnone
  */
 class local_xray_datatableColumn {
-	
-	/**
-	 * Id of column
-	 * @var string
-	 */
-	public $mdata;
-	
-	/**
-	 * Is searchable
-	 * @var boolean
-	 */
-	public $bSearchable;
-	
-	/**
-	 * Text to show
-	 * @var string
-	 */
-	public $text;	
-	
-	/**
-	 * Is sortable
-	 * @var boolean
-	 */
-	public $bSortable;
-	
-	/**
-	 * Show column
-	 * @var boolean
-	 */
-	public $bVisible;
-	
-	/**
-	 * Width column
-	 * @var string
-	 */
-	public $sWidth;
 
-	/**
-	 * Construct
-	 * @param string $mdata - Id of column
-	 * @param string $string - Text to show
-	 */
-	public function __construct($mdata, $text = '', $search = false, $sortable = false, $visible = true, $width = '') {
-		$this->mData = $mdata;
-		$this->text = $text;
-		$this->bSearchable = $search;
-		$this->bSortable = $sortable;
-		$this->bVisible = $visible;
-		$this->sWidth = $width;
-	}
+    /**
+     * Id of column
+     * @var string
+     */
+    public $mdata;
+    
+    /**
+     * Is searchable
+     * @var boolean
+     */
+    public $bSearchable;
+    
+    /**
+     * Text to show
+     * @var string
+     */
+    public $text;
+    
+    /**
+     * Is sortable
+     * @var boolean
+     */
+    public $bSortable;
+    
+    /**
+     * Show column
+     * @var boolean
+     */
+    public $bVisible;
+    
+    /**
+     * Width column
+     * @var string
+     */
+    public $sWidth;
+
+    /**
+     * Construct
+     * @param string $mdata - Id of column
+     * @param string $string - Text to show
+     */
+    public function __construct($mdata, $text = '', $search = false, $sortable = false, $visible = true, $width = '') {
+        $this->mData = $mdata;
+        $this->text = $text;
+        $this->bSearchable = $search;
+        $this->bSortable = $sortable;
+        $this->bVisible = $visible;
+        $this->sWidth = $width;
+    }
 }
 
 /**
@@ -78,81 +78,81 @@ class local_xray_datatableColumn {
  * @author Pablo Pagnone
  */
 class local_xray_datatable {
-	
-	/**
-	 * id for table
-	 * @var string
-	 */
-	public $id;
-	
-	/**
-	 * Url to get data with json format.
-	 * @var string
-	 */
-	public $jsonurl;
-	
-	/**
-	 * Show pager
-	 * @var boolean
-	 */
-	public $paging;
-	
-	/**
-	 * Change Dom
-	 * @var string
-	 */
-	public $dom;
-	
-	/**
-	 * Length menu
-	 * @var array
-	 */
-	public $lengthMenu;
-	
-	/**
-	 * Show search
-	 * @var boolean
-	 */
-	public $search;
-	
-	/**
-	 * Message to process data
-	 * @var string
-	 */
-	public $sProcessingMessage;
-	
-	/**
-	 * Message to show in error case.
-	 * @var string
-	 */
-	public $errorMessage;
-	
-	/**
-	 * Array with objects local_xray_datatableColumn
-	 * @var array
-	 */
-	public $columns;
 
-	/**
-	 * Construct
-	 * 
-	 * @param integer $id
-	 * @param string $jsonurl
-	 * @param array $columns
-	 * @param string $search
-	 * @param string $paging
-	 * @param array $lengthMenu
-	 */
-	public function __construct($id, $jsonurl, array $columns, $search = false, $paging=true, $dom = 'lftipr', $lengthMenu = array(10, 50, 100)) {
-		$this->id = $id;
-		$this->jsonurl = $jsonurl;
-		$this->columns = $columns;
-		$this->search = $search;
-		$this->paging = $paging;
-		$this->dom = $dom;
-		$this->lengthMenu = $lengthMenu;
-		$this->sProcessingMessage = get_string('table_fetchingdata', 'local_xray');
-		$this->errorMessage = get_string('error_datatables','local_xray');
-		
-	}
+    /**
+     * id for table
+     * @var string
+     */
+    public $id;
+    
+    /**
+     * Url to get data with json format.
+     * @var string
+     */
+    public $jsonurl;
+
+    /**
+     * Show pager
+     * @var boolean
+     */
+    public $paging;
+
+    /**
+     * Change Dom
+     * @var string
+     */
+    public $dom;
+
+    /**
+     * Length menu
+     * @var array
+     */
+    public $lengthMenu;
+
+    /**
+     * Show search
+     * @var boolean
+     */
+    public $search;
+
+    /**
+     * Message to process data
+     * @var string
+     */
+    public $sProcessingMessage;
+
+    /**
+     * Message to show in error case.
+     * @var string
+     */
+    public $errorMessage;
+
+    /**
+     * Array with objects local_xray_datatableColumn
+     * @var array
+     */
+    public $columns;
+
+    /**
+     * Construct
+     * 
+     * @param integer $id
+     * @param string $jsonurl
+     * @param array $columns
+     * @param string $search
+     * @param string $paging
+     * @param array $lengthMenu
+     */
+    public function __construct($id, $jsonurl, array $columns, $search = false, $paging=true, $dom = 'lftipr', $lengthMenu = array(10, 50, 100)) {
+        $this->id = $id;
+        $this->jsonurl = $jsonurl;
+        $this->columns = $columns;
+        $this->search = $search;
+        $this->paging = $paging;
+        $this->dom = $dom;
+        $this->lengthMenu = $lengthMenu;
+        $this->sProcessingMessage = get_string('table_fetchingdata', 'local_xray');
+        $this->errorMessage = get_string('error_datatables','local_xray');
+
+    }
 }
