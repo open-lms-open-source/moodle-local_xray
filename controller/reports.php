5603e6583489e5afbd37b9729e3abead2ba06e02
gvitale <german@nxgened.com>
2015-09-01 16:00:03 -0300

    INT-7954: code style 2

--- controller/reports.php
+++ controller/reports.php
@@ -9,52 +9,52 @@ defined('MOODLE_INTERNAL') or die();
  * @package local_xray
  */
 class local_xray_controller_reports extends mr_controller {
-	
-	/**
-	 * Course id
-	 * @var integer
-	 */
-	protected $courseid;
-	
-	/**
-	 * User id
-	 * @var integer
-	 */
-	protected $userid;
-	
-	public function init() {
-		parent::init();
-		if(is_callable('mr_on') && !mr_on("xray", "_MR_LOCAL")) {
-			exit();
-		}
-	}
-	
-	public function setup(){
-		global $PAGE;
-		$PAGE->set_context($this->get_context());
-		parent::setup();
-	}
-	
-	
-	/**
-	 * Require capabilities
-	 */
-	public function require_capability() {
-		require_capability("{$this->plugin}:{$this->name}_view", $this->get_context());
-	}
-    
+
+    /**
+     * Course id
+     * @var integer
+     */
+    protected $courseid;
+
+    /**
+     * User id
+     * @var integer
+     */
+    protected $userid;
+
+    public function init() {
+        parent::init();
+        if(is_callable('mr_on') && !mr_on("xray", "_MR_LOCAL")) {
+            exit();
+        }
+    }
+
+    public function setup(){
+        global $PAGE;
+        $PAGE->set_context($this->get_context());
+        parent::setup();
+    }
+
+
+    /**
+     * Require capabilities
+     */
+    public function require_capability() {
+        require_capability("{$this->plugin}:{$this->name}_view", $this->get_context());
+    }
+
     /**
      * Show data of last request to webservice xray.
      */
     public function debugwebservice() {
-    	
-    	echo "<pre>";
-    	var_dump(\local_xray\api\xrayws::instance()->geterrorcode());
-    	var_dump(\local_xray\api\xrayws::instance()->geterrormsg());
-    	var_dump(\local_xray\api\xrayws::instance()->lastresponse());
-    	var_dump(\local_xray\api\xrayws::instance()->lasthttpcode());
-    	var_dump(\local_xray\api\xrayws::instance()->getinfo());
-    	echo "</pre>";
-    	exit();
+
+        echo "<pre>";
+        var_dump(\local_xray\api\xrayws::instance()->geterrorcode());
+        var_dump(\local_xray\api\xrayws::instance()->geterrormsg());
+        var_dump(\local_xray\api\xrayws::instance()->lastresponse());
+        var_dump(\local_xray\api\xrayws::instance()->lasthttpcode());
+        var_dump(\local_xray\api\xrayws::instance()->getinfo());
+        echo "</pre>";
+        exit();
     }
 }
