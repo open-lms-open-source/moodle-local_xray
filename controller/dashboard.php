8edf3650004aa5ed37a0bc741eb240f530e41073
gvitale <german@nxgened.com>
2015-09-10 13:34:40 -0300

    INT-8072: dashboard course header;

--- controller/dashboard.php
+++ controller/dashboard.php
@@ -10,54 +10,26 @@ require_once($CFG->dirroot.'/local/xray/controller/reports.php');
  * @package local_xray
  */
 class local_xray_controller_dashboard extends local_xray_controller_reports {
-		
-	public function init() {
-		parent::init();
-		$this->courseid = required_param('courseid', PARAM_RAW);		
-	}	
-	
-    public function view_action() {
-    	
-    	global $PAGE, $USER, $DB;
-    	
-    	$output = "";
 
-    	try {
-    		$report = "dashboard";
-    		$response = \local_xray\api\wsapi::course($this->courseid, $report);
+    public function init() {
+        parent::init();
+        $this->courseid = required_param('courseid', PARAM_RAW);		
+    }
 
-    		if(!$response) {
-    			// Fail response of webservice.
-    			\local_xray\api\xrayws::instance()->print_error();
-    			
-    		} else {
-    			
-    			// Get users in risk.
-    			$users_in_risk = array();
-    			if(isset($response->elements[1]->data) && !empty($response->elements[1]->data)) {
-    				foreach($response->elements[1]->data as $key => $obj) {
-    					if($obj->severity->value == "high") {
-    						$users_in_risk[] = $obj->participantId->value;
-    					}
-    				}    				
-    			}
-    			
-    			$count_students_risk = (isset($response->elements[4]->items[5]->value) ? $response->elements[4]->items[5]->value : "-");
-    			$count_students_enrolled = (isset($response->elements[4]->items[2]->value) ? $response->elements[4]->items[2]->value : "-");
-    			$count_students_visits_lastsevendays = (isset($response->elements[4]->items[0]->value) ? $response->elements[4]->items[0]->value : "-");
-    			
-    			// TODO:: Get list of students in risk (Pending in webservice).
-    			
-    			
-    			//TODO:: Call to renderer
-    			$output .= "";
-		    	
-    		}		 
-    	} catch(exception $e) {
-    		print_error('error_xray', $this->component,'',null, $e->getMessage());
-    	}
-    	
-    	return $output;
+public function view_action() {
+        
+        global $PAGE, $DB;
+        
+        $title = get_string($this->name, $this->component);
+        $PAGE->set_title($title);
+        $this->heading->text = $title;
+        
+        // Add title to breadcrumb.
+        $PAGE->navbar->add(get_string($this->name, $this->component));
+        
+        $output = $this->output->course_header($this->courseid);
+        
+        return $output;
     }
     
 }
