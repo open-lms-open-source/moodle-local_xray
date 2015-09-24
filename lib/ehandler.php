cf14032926c61f8a9aa1108d11e129c8be39b096
Darko Miletic <darko.miletic@gmail.com>
2015-09-24 11:08:08 -0300

    INT-7954, INT-8182: Changed json error handling #2

--- lib/ehandler.php
+++ lib/ehandler.php
@@ -6,14 +6,9 @@ defined('MOODLE_INTERNAL') or die();
  * @throws coding_exception
  */
 function local_xray_default_exception_handler(Exception $e) {
-    global $PAGE, $ME;
-    $url = new moodle_url('/local/xray/view.php');
-    $current = new moodle_url($ME);
-    $validurl = ($url->compare($current, URL_MATCH_BASE));
-    $courseid = optional_param('courseid', false, PARAM_INT);
-    $controller = optional_param('controller', false, PARAM_ALPHA);
     $action = optional_param('action', 'view', PARAM_ALPHANUMEXT);
-    if ($validurl and $courseid and $controller and $action and stripos($action, 'json') === 0) {
+    if ($action and (stripos($action, 'json') === 0)) {
+        global $PAGE;
         $output = new core_renderer_ajax($PAGE, RENDERER_TARGET_AJAX);
         echo $output->header(), json_encode(array('data' => '-'));
         return;
