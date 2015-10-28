7f8212b6dd63712001d76dbe22c49ca51229765f
Pablo Pagnone <pablo@nxgened.com>
2015-10-28 10:38:49 -0300

    INT-8346: Refactor JSON calls

---
+++ rest.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+* Rest json controller
+*
+* @package   local_xray
+* @copyright Copyright (c) 2015 Moodlerooms Inc. (http://www.moodlerooms.com)
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+define('AJAX_SCRIPT', true);
+define('NO_DEBUG_DISPLAY', true);
+
+require_once('../../config.php');
+/* @var stdClass $CFG */
+require($CFG->dirroot.'/local/mr/bootstrap.php');
+
+mr_controller::render('local/xray', 'pluginname', 'local_xray');
