824f0e4b2af1e7366b6bdcf1ef5f3960ea6ce85f
gvitale <german@nxgened.com>
2015-12-01 16:43:37 -0300

    INT-8211: downgrade version because Darko will need to do it in his branch

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 /* @var stdClass $plugin */
-$plugin->version  = 2015070323;
+$plugin->version  = 2015070322;
 $plugin->requires = 2014051200; // Moodle 2.7 .
 $plugin->cron = 0;
 $plugin->component = 'local_xray';
