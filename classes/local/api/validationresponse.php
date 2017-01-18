82cfb426706c866deb0e94f34afbca8f380bbd0b
David Castro <david.castro@blackboard.com>
2017-01-18 06:21:40 -0500

    INT/9148: Fixed coding standards

--- classes/local/api/validationresponse.php
+++ classes/local/api/validationresponse.php
@@ -33,15 +33,46 @@ defined('MOODLE_INTERNAL') || die();
  */
 class validationresponse {
 
+    /**
+     * Holds the result if any errors are registered.
+     * @var array Error results.
+     */
     private $result;
-    private $reason;
-    private $reasonfields;
-    private $checkkey;
-    private $steps;
-    private $currStep;
 
     /**
-     * Constructor method
+     * Describes the last reason for an error.
+     * @var string Last reason for error.
+     */
+    private $reason;
+
+    /**
+     * Holds the fields that may have caused an error.
+     * @var array Error causing fields.
+     */
+    private $reasonfields;
+
+    /**
+     * Identifies the calling check.
+     * @var string Key that identifies the check corresponding to this response.
+     */
+    private $checkkey;
+
+    /**
+     * Number of steps for this validation.
+     * @var int Number of steps for this validation.
+     */
+    private $steps;
+
+    /**
+     * Current step in validation.
+     * @var int Current step.
+     */
+    private $currstep;
+
+    /**
+     * Constructor method.
+     * @param string $checkkey Key that identifies the check corresponding to this response.
+     * @param int $steps Number of steps for this validation.
      */
     public function __construct($checkkey, $steps) {
         $this->result = array();
@@ -49,35 +80,62 @@ class validationresponse {
         $this->reasonfields = array();
         $this->checkkey = $checkkey;
         $this->steps = $steps;
-        $this->currStep = 0;
+        $this->currstep = 0;
     }
 
+    /**
+     * Adds a result to the response.
+     * @param string $res New result.
+     */
     public function add_result($res) {
         $this->result[] = $res;
     }
 
+    /**
+     * Returns the result of the response.
+     * @return array Array of strings with result.
+     */
     public function get_result() {
         return $this->result;
     }
 
+    /**
+     * Checks if the validation is successful so far.
+     * @return bool true if there were no results, false otherwise.
+     */
     public function is_successful() {
         return empty($this->result);
     }
 
+    /**
+     * Array of string identifiers for the fields that may have caused an error.
+     * @param array $reasonfields Array of field identifiers.
+     */
     public function set_reason_fields($reasonfields) {
         $this->reasonfields = $reasonfields;
     }
 
+    /**
+     * Set the reason for which this validation may have failed in the current step.
+     * @param string $reason Reason message.
+     */
     public function set_reason($reason) {
         $this->reason = $reason;
     }
-    
+
+    /**
+     * Increase the current step.
+     */
     public function step() {
-        $this->currStep ++;
+        $this->currstep ++;
     }
 
+    /**
+     * Checks if the validation is finished.
+     * @return bool true if all steps have been accounted for, false otherwise.
+     */
     public function is_finished() {
-        return $this->currStep === $this->steps;
+        return $this->currstep === $this->steps;
     }
 
     /**
@@ -102,7 +160,7 @@ class validationresponse {
 
     /**
      * Wraps string with a strong html tag
-     * @param string $str
+     * @param string $str String to wrap.
      */
     private function strong($str) {
         return '<strong>'.$str.'</strong>';
@@ -110,8 +168,8 @@ class validationresponse {
 
     /**
      * Wraps string with a div html tag with a serviceinfo class
-     * @param string $service
-     * @param string $str
+     * @param string $service Service identifier.
+     * @param string $str Message string.
      */
     private function service_info($service, $str) {
         $responsetitle = get_string('validate_service_response', wsapi::PLUGIN);
@@ -122,21 +180,21 @@ class validationresponse {
 
     /**
      * Registers an error based on step and optional message
-     * @param string $exkey
-     * @param string $message
+     * @param string $exkey Service identifier.
+     * @param string $message Message string.
      */
     public function register_error($exkey, $message = null) {
         $break = '<br />';
         $colon = ': ';
-        $numStep = $this->currStep + 1;
-        $stepsstr = "($numStep/$this->steps)";
+        $numstep = $this->currstep + 1;
+        $stepsstr = "($numstep/$this->steps)";
         $htmlfields = $this->list_fields($this->reasonfields);
         $whenstr = get_string('validate_when', wsapi::PLUGIN).' ';
         $fieldstitle = get_string('validate_check_fields', wsapi::PLUGIN).$colon.$break;
         $msgstr = $this->service_info($this->checkkey, isset($message) ? $message : $stepsstr);
 
         $this->add_result(get_string("error_".$exkey."_exception",
-            wsapi::PLUGIN, $whenstr.$this->strong($this->reason).$break
-            .$fieldstitle.$htmlfields.$msgstr));
+                wsapi::PLUGIN, $whenstr.$this->strong($this->reason).$break
+                .$fieldstitle.$htmlfields.$msgstr));
     }
 }
