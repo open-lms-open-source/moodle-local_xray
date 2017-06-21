8bf3e2934e7e76fe0af03d3e69c7bd8662e4652d
gvitale <german@nxgened.com>
2017-06-21 10:41:30 -0300

    INT-11292: js fixed

--- jquery/reports/dashboard.js
+++ jquery/reports/dashboard.js
@@ -3,12 +3,12 @@
  */
 function local_xray_recommendations_show() {
     $(document).ready(function () {
-        icon = $("#xray-div-recommendations-icon");
         // Get element to display.
         content = $("#xray-div-recommendations-show");
         // Open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
         content.slideToggle(500, function () {
             // Execute this after slideToggle is done.
+            icon = $("#xray-div-recommendations-icon");
             var iconClass = icon.attr("class");
             if (iconClass == 'countrecommendedactions_icon_expand') {
                 icon.addClass('countrecommendedactions_icon_collapse').removeClass('countrecommendedactions_icon_expand')
@@ -26,11 +26,11 @@ function local_xray_recommendations_show() {
  */
 function local_xray_headline_show() {
     $(document).ready(function () {
-        icon = $("#xray-div-headline-icon");
         // Get element to display.
         content = $("#xray-div-headline-show");
         // Open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
         content.slideToggle(500, function () {
+            icon = $("#xray-div-headline-icon");
             // Execute this after slideToggle is done.
             var iconClass = icon.attr("class");
             if (iconClass == 'headline_icon_expand') {
